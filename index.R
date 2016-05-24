de9e853aeaefb4925b0b78767210459798aee61e
Maggie <mcm710@gmail.com>
2016-05-24 15:18:24 -0400

    indexing

---
+++ index.R
@@ -0,0 +1,24 @@
+library(dplyr)
+package_downloads$package <- as.character(package_downloads$package)
+z <- numeric(length(package_downloads))
+for(i in 1:1000){
+  message(i)
+  x <- package_downloads[i,2]
+  a<-rapply(package_dependencies(package_downloads$package[i], which=c("Depends", "Imports"), reverse=TRUE), as.character)
+  if(length(a)>0){
+    names(a) <- NULL
+    for(j in 1:length(a)){
+      if(a[j] %in% package_downloads$package){
+        b <- filter(package_downloads, package==a[j])[,2]
+        x <- x-b
+      }
+    }
+  }
+  z[i] <- x
+}
+
+index <- data.frame(package=head(package_downloads$package,1000), i=z)
+index <- mutate(index, raw_rank=c(1:1000))
+index <- arrange(index, desc(i))
+index <- mutate(index, index_rank=c(1:1000))
+
